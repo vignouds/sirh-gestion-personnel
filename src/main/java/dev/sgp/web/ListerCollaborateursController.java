0a9ae1b46dcc978f43b906b2e7e044b2ab9d493c
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-05-22 17:00:56 +0200

    creation premiere jsp

--- src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -1,6 +1,7 @@
 package dev.sgp.web;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,16 +12,7 @@ public class ListerCollaborateursController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
 	ServletException, IOException {
-		// recupere la valeur d'un parametre dont le nom est avecPhoto
-		String avecPhotoParam = req.getParameter("avecPhoto");
-		
-		String departementParam = req.getParameter("departement");
-		resp.setContentType("text/html");
-		
-		resp.getWriter().write("<h1>Liste des collaborateurs</h1>"
-				+"<ul>"
-				+"<li>avecPhoto="+avecPhotoParam+"</li>"
-				+"<li>departement="+departementParam+"</li>"
-				+"<ul>");
+		req.setAttribute("listeNoms", Arrays.asList("Robert","Jean","Hugues"));
+		req.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp").forward(req, resp);
 	}
 }
